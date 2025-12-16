8c2f46a8b5fa01bfb130ae88f2222d59137d4850
JoksLobo <jocson_herry@hotmail.com>
2025-12-16 16:50:11 -0300

    construção dos controles do shadow box

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,42 @@
 import styles from "./styles.css";
 
 export default function Home() {
-  return <div>olá, mundo</div>;
+  return (
+    <div>
+      <h1>Gerador de Box-shadow</h1>
+      <main className="container">
+        <div className="controls">
+          <div className="control-group">
+            <label htmlFor="offsetX">Deslocamento horizontal</label>
+            <input type="range" id="offsetX" min={-100} max={100} />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="offsetY">Deslocamento vertical</label>
+            <input type="range" id="offsetY" min={-100} max={100} />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="blur">Desfoque (Blur)</label>
+            <input type="range" id="blur" min={0} max={100} />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="spread">Tamanho (Spread)</label>
+            <input type="range" id="spread" min={-50} max={50} />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="color">Cor</label>
+            <input type="color" id="color" />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="opacity">Opacidade</label>
+            <input type="range" id="opacity" min={0} max={1} step={0.01} />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
